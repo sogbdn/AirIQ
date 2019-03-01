8ea48fe0631ac8b5a97393302e481cf6446c20fd
sogbdn <sonia.barretdenazaris@gmail.com>
2019-03-01 22:35:27 +0000

    updated migrations and seeds

--- db/migrations/20190301191941_create_users.js
+++ db/migrations/20190301191941_create_users.js
@@ -1,12 +1,19 @@
 /* make sure to rollback before migrating or making changes */
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary();
-    table.string('name');
-    /* add actual table columns here */
-  });
+	return knex.schema.createTable('users', function(table) {
+		table.increments('id').primary();
+		table.string('first_name');
+		table.string('last_name');
+		table.string('email');
+		table.string('phone_number');
+		table.string('password');
+		table.string('password_conf');
+		table.string('profile_type');
+		table.string('sms_good_days');
+		table.string('sms_bad_days');
+	});
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+	return knex.schema.dropTable('users');
 };
