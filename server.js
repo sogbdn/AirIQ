b216fbd03f1974117b96f410b5fbc1207833a549
qwhat <justinmao@live.ca>
2019-03-01 19:51:20 +0000

    add knex config

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const path = require('path')
 const methods = require('methods')
 const express = require('express')
 const bodyParser = require('body-parser')
+const knexConfig = require('./knexfile')
+const env = 'development'
+const knex = require('knex')(knexConfig[env])
 const app = express()
+
+require("dotenv").config()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
