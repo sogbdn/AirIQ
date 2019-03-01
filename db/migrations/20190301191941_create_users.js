61a859e97fe2578f102230b5c0bd7f92a381c619
qwhat <justinmao@live.ca>
2019-03-01 20:21:55 +0000

    rename migration file to meet coding norms

--- db/migrations/20190301191941_create_users.js
+++ db/migrations/20190301191941_create_users.js
@@ -1,8 +1,9 @@
-
+/* make sure to rollback before migrating */
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments('id').primary();
     table.string('name');
+    /* add actual table columns here */
   });
 };
 
