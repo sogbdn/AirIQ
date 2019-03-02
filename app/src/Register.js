1e95ed64f920b5a2edcdd278f6cab53b5800171e
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-02 13:13:53 -0500

    fixed bootstrap conflict was erroneously installed in npm dev now corr

---
+++ app/src/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Register extends Component {
+
+  componentDidMount() {
+    console.log("Register Mounted");
+  }
+  render() {
+    return (
+      <div>hello world</div>
+      
+    );
+  }
+}
+
+export default Register;
