06e3003764e84351051d5fc2635dac154ed0f487
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-05 23:20:51 +0000

    adding again seems nested files arent grabbed in a git add .

--- client/src/components/partials/_AirCard.js
+++ client/src/components/partials/_AirCard.js
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import AirRatingRecom  from './_AirRatingRecom'
 
 
 
@@ -14,18 +15,30 @@ export default class AirCard extends Component {
     console.log("AirCard Mounted");
   }
 
- 
-
   qualityColor(airquality){
     if (airquality < 50){
       return "alert1"
+    }
+    else if (airquality > 50 && airquality < 100){
+      return "alert2"
+    }
+    else if (airquality < 150 && airquality > 100){
+      return "alert3"
+    }
+    else if (airquality > 150 && airquality < 200){
+      return "alert4"
+    }
+    else if (airquality > 200 && airquality <300){
+      return "alert5"
+    }
+    else {
+      return "unavailable"
     }
   }
 //separate file create and object with and object
   //export const airstrings = {"AQ1": "something"}
   //airSTrings and try to access the object    AQ + props
 
-  airstr
 
 
 
@@ -41,7 +54,10 @@ export default class AirCard extends Component {
           <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_black.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2">Air Quality: Good</Card.Subtitle></Col>
           <Col lg={10} md= {9} sm={8} xs={6}>
           <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-          <div className="Aqi_num"><p className="aqinum">{this.props.airQuality + 4}</p>asdifjoasidjf oijoafidsj ojasdf oiajsdofjio oasdfiojioasd jfoijadosifj oadsjfoij oiasdofjoi asdfoj oasdfojasdo ifjoiasdjf oisajdofj jioa </div>
+          <div className="Aqi_num"><p className="aqinum">{this.props.airQuality}</p>
+          <p><AirRatingRecom airRating = {this.qualityColor(this.props.airQuality)}/></p>
+          
+          </div>
           </Col>
         </Row>
         <Row>
