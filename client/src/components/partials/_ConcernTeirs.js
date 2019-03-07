a683a3c5aba7df9fcd1b5508b3eb781ba0330156
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-07 17:54:59 +0000

    adding new page for health concern info

---
+++ client/src/components/partials/_ConcernTeirs.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import AirRatingRecom  from './_AirRatingRecom'
+import Image from 'react-bootstrap/Image'
+
+
+///
+
+export default class ConcernTeirs extends Component {
+
+  componentDidMount() {
+    console.log("Health Concern Teirs");
+  }
+
+}
