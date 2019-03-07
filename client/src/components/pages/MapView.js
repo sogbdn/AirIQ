12a76238ad5cfe4ffe059d3df84facee5c35faaf
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-07 00:36:43 +0000

    adding custom markers

--- client/src/components/pages/MapView.js
+++ client/src/components/pages/MapView.js
@@ -10,9 +10,9 @@ export class MapView extends Component {
     selectedPlace: {marker: {}},
     points: [
       { id: 1, lat: 45.496338, lng: -73.570732, airiq: 57 },
-      { id: 2, lat: 42.03, lng: -73.670732, airiq: 73 },
-      { id: 3, lat: 41.03, lng: -73.560932, airiq: 33 },
-      { id: 4, lat: 42.05, lng: -73.550732, airiq: 45 }
+      { id: 2, lat: 45.496338, lng: -73.670732, airiq: 73 },
+      { id: 3, lat: 45.496338, lng: -73.560932, airiq: 33 },
+      { id: 4, lat: 45.496338, lng: -73.550732, airiq: 45, icon: "/marker_1.png" }
     ]
   };
 
@@ -53,25 +53,12 @@ export class MapView extends Component {
         onClick={this.onMapClicked}
         initialCenter={{lat: 45.496338, lng: -73.570732}}
         >
-        {this.state.points.map(p => <Marker marker={{'airiq': p.airiq}} key={p.id} onClick={this.onMarkerClick} position={{lat: p.lat, lng: p.lng}} />)}
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
+        {this.state.points.map(p => <Marker marker={{'airiq': p.airiq}} key={p.id} onClick={this.onMarkerClick} position={{lat: p.lat, lng: p.lng}} icon={{url:"/marker_1.png"}} />)}
+        <InfoWindow visible={this.state.showingInfoWindow} marker={this.state.activeMarker}>
             <div>
-              <h1>{this.state.selectedPlace.marker.airiq}</h1>
+              <p>{this.state.selectedPlace.marker.airiq}</p>
             </div>
         </InfoWindow> 
-        {/* <Marker onClick={this.onMarkerClick}
-          name={'Current location'}
-          position={{lat: 45.496338, lng: -73.570732}} //first to say this is javascript the second to say this is an object
-          />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-              <div>
-                <h1>{this.props.airQuality}</h1>
-              </div>
-          </InfoWindow> */}
         </Map>
         </div>
       //);
