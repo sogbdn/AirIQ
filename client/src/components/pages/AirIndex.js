9a46a50cd5f1ec0dbe5352ddf1beaab80623de2c
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-07 06:53:26 +0000

    adding icon options filling air index info page in particular working on essentializing image layout but far from finished

--- client/src/components/pages/AirIndex.js
+++ client/src/components/pages/AirIndex.js
@@ -6,6 +6,7 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container'
 import Table from 'react-bootstrap/Table'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
 //page not actually defined yet
 export default class AirIndex extends Component {
@@ -14,17 +15,31 @@ export default class AirIndex extends Component {
   }
   render() {
     return (
-  <Container>
+<Container>
+<Card className="introcard">
+  <Card.Body>
+  <Row>
+    <Col>
+    <p className="white">Think of the AQI as a yardstick that runs from 0 to 500. The higher the AQI value, the greater the level of air pollution and the greater the health concern. For example, an AQI value of 50 represents good air quality with little potential to affect public health, while an AQI value over 300 represents hazardous air quality.</p>
+    <div align="right">
+    <div className="cardicons"><Image src="kite.svg" className="kite"/></div>
+    </div>
+    </Col>
+    </Row>
+  </Card.Body>
+</Card>
 
 <Card className="alert1">
   <Card.Body>
   <Row>
     <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_black.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
-    <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqinum">0-50</p>
-    <p className="aqiheadline">Breath deep and travel far and wide.</p>
+    <Card.Subtitle className="mb-2">Air Quality Index: 0-50</Card.Subtitle>
+    <div className="Aqi_num"><p className="aqiheadline">Breath deep and travel far and wide.</p></div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="forest.svg"/></div>
     </div>
+    
     </Col>
     </Row>
     <Row>
@@ -45,9 +60,11 @@ export default class AirIndex extends Component {
   <Row>
     <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_black.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
-    <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqinum">51-100</p>
-    <p className="aqiheadlinedark">A okay day for shorter excursions, but maybe delay the marathon</p>
+    <Card.Subtitle className="mb-2">Air Quality Index: 51-100</Card.Subtitle>
+    <div className="Aqi_num">
+    <p className="aqiheadlinedark">An okay day for short excursions, but maybe delay the marathon</p> </div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="bench.svg"/></div>
     </div>
     </Col>
     </Row>
@@ -59,9 +76,12 @@ export default class AirIndex extends Component {
   <Row>
     <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_black.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
-    <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqinum">51-100</p>
-    <p className="aqiheadlinedark">Hmmm, might be a good day for indoor activities. It's a tough day outside, for the lungs and heart.</p>
+    <Card.Subtitle className="mb-2">Air Quality Index: 101-150</Card.Subtitle>
+    <div className="Aqi_num">
+    <p className="aqiheadlinedark">...Might be a good day for indoor activities. It's a tough day outside, for the lungs and heart.</p>
+    </div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="bonsai.svg"/></div>
     </div>
     </Col>
     </Row>
@@ -71,25 +91,31 @@ export default class AirIndex extends Component {
 <Card className="alert4">
   <Card.Body>
   <Row>
-    <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_white.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
+    <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_white.png" className ="avatar" bsPrefix/></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
-    <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqinum">51-100</p>
-    <p className="aqiheadlinedark">Take it easy and take shelter. Limit outdoor time or heavy exersions where possible</p>
+    <Card.Subtitle className="mb-2">Air Quality Index: 151-200</Card.Subtitle>
+    <div className="Aqi_num">
+    <p className="aqiheadlinedark">Take it easy and take shelter. Limit outdoor time or heavy exersions, where possible</p>
+    </div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="whistle.svg"/></div>
     </div>
     </Col>
     </Row>
   </Card.Body>
 </Card>
 
-<Card className="alert4">
+<Card className="alert5">
   <Card.Body>
   <Row>
     <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_white.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
-    <Card.Subtitle className="mb-2">Air Quality Index</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqinum">51-100</p>
-    <p className="aqiheadlinedark">Take it easy and take shelter. Limit outdoor time or heavy exersions where possible</p>
+    <Card.Subtitle className="mb-2" className="aqiwhitenum">Air Quality Index: 201-300</Card.Subtitle>
+    <div className="Aqi_num">
+    <p className="aqiheadline">Significant health risk present. Take care and stay indoors</p>
+    </div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="roadblock.svg"/></div>
     </div>
     </Col>
     </Row>
@@ -97,44 +123,23 @@ export default class AirIndex extends Component {
 </Card>
 
 
-
-
+<Card className="alert6">
+  <Card.Body>
   <Row>
-    <Col><p>Think of the AQI as a yardstick that runs from 0 to 500. The higher the AQI value, the greater the level of air pollution and the greater the health concern. For example, an AQI value of 50 represents good air quality with little potential to affect public health, while an AQI value over 300 represents hazardous air quality.</p></Col>
-  </Row>
+    <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_white.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
+    <Col lg={10} md= {9} sm={8} xs={6}>
+    <Card.Subtitle className="mb-2" className="aqiwhitenum">Air Quality Index: 201-300</Card.Subtitle>
+    <div className="Aqi_num">
+    <p className="aqiheadline">Hazardous. Serious lung and heart aggravation. Everyone should avoid all outdoor exertion</p>
+    </div>
+    <div align="right">
+    <div className="cardicons" align="right"><Image src="saviour.svg"/></div>
+    </div>
+    </Col>
+    </Row>
+  </Card.Body>
+</Card>
 
-  <Table bordered hover size="sm">
-  <thead>
-    <tr>
-      <th >Air Quality Guide</th>
-      <th>Good (0 - 50)</th>
-      <th>Moderate (51- 100)</th>
-      <th>Unhealthy for Sensitive Groups (101 - 150)</th>
-      <th>Unhealthy (151 - 200)</th>
-      <th>Very Unhealthy (200 - 300)</th>
-      <th>Hazardous (301 - 500)</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Good</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</Table>
  </Container>
     );
   }
