624ce2f723a28ebc056f33b8608b5d53e91f8c1d
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-07 20:41:55 +0000

    successfully added a new styling all the syntax has to be changed from the documentation tricky tricky

--- client/src/components/pages/MapView.js
+++ client/src/components/pages/MapView.js
@@ -60,9 +60,12 @@ export class MapView extends Component {
         <div>{this.props.airQuality}</div>
         <Map google={this.props.google} zoom={14}
         onClick={this.onMapClicked}
+        styles= {[{ stylers: [{ 'saturation': -100 }, { 'gamma': 0.8 }, { 'lightness': 4 }] }]}
+
         initialCenter={{lat: 45.496338, lng: -73.570732}}
         >
-        {this.state.points.map(p => <Marker marker={{'airiq': p.airiq}} key={p.id} onClick={this.onMarkerClick} position={{lat: p.lat, lng: p.lng}} icon={{url:p.icon}} />)}
+        {this.state.points.map(p => <Marker marker={{'airiq': p.airiq}} key={p.id} onClick={this.onMarkerClick} position={{lat: p.lat, lng: p.lng}} icon={{url:p.icon}} 
+        />)}
         <InfoWindow visible={this.state.showingInfoWindow} marker={this.state.activeMarker}>
             <div>
               <p>{this.state.selectedPlace.marker.airiq}</p>
