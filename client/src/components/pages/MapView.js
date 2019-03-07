9fde677eb48873cc41554bcbf47d6f9b7d34e401
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-07 00:10:02 +0000

    added mulitple markers to map view testing static data from test object that can potentially show different air iq amounts

--- client/src/components/pages/MapView.js
+++ client/src/components/pages/MapView.js
@@ -4,21 +4,33 @@ import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapView extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {marker: {}},
+    points: [
+      { id: 1, lat: 45.496338, lng: -73.570732, airiq: 57 },
+      { id: 2, lat: 42.03, lng: -73.670732, airiq: 73 },
+      { id: 3, lat: 41.03, lng: -73.560932, airiq: 33 },
+      { id: 4, lat: 42.05, lng: -73.550732, airiq: 45 }
+    ]
+  };
 
-    state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-    };
+  
+  // <Marker onClick={this.onMarkerClick}
+  //         name={'Current location'}
+  //         position={{lat: 45.496338, lng: -73.570732}} //first to say this is javascript the second to say this is an object
+  //         />
 
-    
 
-    onMarkerClick = (props, marker, e) =>
+    onMarkerClick = (props, marker, e) => {
+      console.log(props)
       this.setState({
         selectedPlace: props,
         activeMarker: marker,
         showingInfoWindow: true
       });
+    }
 
       onMapClicked = (props) => {
         if (this.state.showingInfoWindow) {
@@ -35,21 +47,23 @@ export class MapView extends Component {
     render() {
 
       {return !this.props.displaymap ? null :
-
-      
        <div>
         <div>{this.props.airQuality}</div>
         <Map google={this.props.google} zoom={14}
         onClick={this.onMapClicked}
         initialCenter={{lat: 45.496338, lng: -73.570732}}
         >
-        <Marker onClick={this.onMarkerClick}
+        {this.state.points.map(p => <Marker marker={{'airiq': p.airiq}} key={p.id} onClick={this.onMarkerClick} position={{lat: p.lat, lng: p.lng}} />)}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.marker.airiq}</h1>
+            </div>
+        </InfoWindow> 
+        {/* <Marker onClick={this.onMarkerClick}
           name={'Current location'}
           position={{lat: 45.496338, lng: -73.570732}} //first to say this is javascript the second to say this is an object
-          />
-          <Marker onClick={this.onMarkerClick}
-          name={'Current location'}
-          position={{lat: this.props.latitude, lng: this.props.longitude}} //first to say this is javascript the second to say this is an object
           />
           <InfoWindow
             marker={this.state.activeMarker}
@@ -57,7 +71,7 @@ export class MapView extends Component {
               <div>
                 <h1>{this.props.airQuality}</h1>
               </div>
-          </InfoWindow>
+          </InfoWindow> */}
         </Map>
         </div>
       //);
--- client/src/components/pages/UserProfile.js
+++ client/src/components/pages/UserProfile.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";;
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
 
 //page not actually defined yet
 export default class UserProfile extends Component {
@@ -6,10 +7,45 @@ export default class UserProfile extends Component {
     console.log("UserProfile Mounted");
   }
   render() {
+
+    var users = [
+      {
+        id: 1,
+        first_name: "Laura",
+        last_name: "Palmer",
+        email: "laura.palmer@gmail.com",
+        password: "blackjack",
+        password_confirmation: "blackjack",
+        profile_type: 1,
+        sms_good_days: false,
+        sms_bad_days: true
+      },
+      {
+        id: 2,
+        first_name: "Dale",
+        last_name: "Cooper",
+        email: "coffelover4ever@gmail.com",
+        password: "cherrypie",
+        password_confirmation: "cherrypie",
+        profile_type: 2,
+        sms_good_days: true,
+        sms_bad_days: false
+      }
+    ];
+    
+    for ( var prop in users){
+
+      console.log(users[prop].first_name)
+     
+    }
+
+    
     return (
-      <div>
-        UserProfile placeholder
-      </div>
+    
+        <Container>
+       something
+        </Container>
+      
     );
   }
 }
