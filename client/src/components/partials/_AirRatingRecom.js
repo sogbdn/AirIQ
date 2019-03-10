07f0fb5cc1a5f73e924a2bae3c1259720a752c36
cepholopoddreamz <leslie.plumb@gmail.com>
2019-03-10 19:05:10 +0000

    adding conditional checked for logged in users and if their concern tier is lifestyle or medical

--- client/src/components/partials/_AirRatingRecom.js
+++ client/src/components/partials/_AirRatingRecom.js
@@ -36,8 +36,8 @@ export default class AirCard extends Component {
         bad_days: decodedUser.sms_bad_days,
         good_days: decodedUser.sms_good_days
       })
+      console.log(this.state);
     });
-    console.log(this.state);
   }
     
   }
@@ -48,9 +48,71 @@ export default class AirCard extends Component {
     AQI4: "Take it easy and take shelter. Limit outdoor time or heavy exersions, where possible",
     AQI5: "Significant health risks. Avoid prolonged exposure outside",
     AQI6: "Hazardous. Serious lung and heart aggravation. Everyone should avoid all outdoor exertion",
-    AQI_null: "Service tower unavailable for this area"
+    AQI_null: "Service tower unavailable for this area",
+    AQI1_medical : "medical test1",
+    AQI2_medical : "medical test2",
+    AQI3_medical : "medical test3",
+    AQI4_medical : "medical4",
+    AQI5_medical : "medical5",
+    AQI1_lifestyle : "lifestyle test1",
+    AQI2_lifestyle : "lifestyle test2",
+    AQI3_lifestyle : "lifestyle test3",
+    AQI4_lifestyle : "lifestyle test4",
+    AQI5_lifestyle : "lifestyle test5",
   }
-  extractrating(rating){
+
+  cardResponse(rating){
+  const currentUser = localStorage.getItem('token');
+  if (currentUser !== null){
+    if (rating === "alert1"){
+      if(this.state.concern_type === "medical"){
+      return this.airRatingStrings.AQI1_medical
+      }
+      else if(this.state.concern_type === "lifestyle"){
+        return this.airRatingStrings.AQI1_lifestyle
+      }
+    }
+    else if (rating === "alert2"){
+      if(this.state.concern_type === "medical"){
+        return this.airRatingStrings.AQI2_medical
+        }
+        else if(this.state.concern_type === "lifestyle"){
+          return this.airRatingStrings.AQI2_lifestyle
+        }
+    }
+    else if (rating === "alert3"){
+      if(this.state.concern_type === "medical"){
+        return this.airRatingStrings.AQI3_medical
+        }
+        else if(this.state.concern_type === "lifestyle"){
+          return this.airRatingStrings.AQI3_lifestyle
+        }
+    }
+    else if (rating === "alert4"){
+      if(this.state.concern_type === "medical"){
+        return this.airRatingStrings.AQI4_medical
+        }
+        else if(this.state.concern_type === "lifestyle"){
+          return this.airRatingStrings.AQI4_lifestyle
+        }
+    }
+    else if (rating === "alert5"){
+      if(this.state.concern_type === "medical"){
+        return this.airRatingStrings.AQI5_medical
+        }
+        else if(this.state.concern_type === "lifestyle"){
+          return this.airRatingStrings.AQI5_lifestyle
+        }
+    }
+    else if (rating === "alert6"){
+      if(this.state.concern_type === "medical"){
+        return this.airRatingStrings.AQI6
+        }
+        else if(this.state.concern_type === "lifestyle"){
+          return this.airRatingStrings.AQI6
+        }
+    }
+  }else if (currentUser === null || this.state.concern_type === "general"){
     if (rating === "alert1"){
       return this.airRatingStrings.AQI1
     }
@@ -69,7 +131,32 @@ export default class AirCard extends Component {
     else if (rating === "unavailable"){
       return this.airRatingStrings.AQI_null
     }
+
   }
+}
+
+
+
+  // extractrating(rating){
+  //   if (rating === "alert1"){
+  //     return this.airRatingStrings.AQI1
+  //   }
+  //   else if (rating === "alert2"){
+  //     return this.airRatingStrings.AQI2
+  //   }
+  //   else if (rating === "alert3"){
+  //     return this.airRatingStrings.AQI3
+  //   }
+  //   else if (rating === "alert4"){
+  //     return this.airRatingStrings.AQI4
+  //   }
+  //   else if (rating === "alert5"){
+  //     return this.airRatingStrings.AQI5
+  //   }
+  //   else if (rating === "unavailable"){
+  //     return this.airRatingStrings.AQI_null
+  //   }
+  // }
 
 //separate file create and object with and object
   //export const airstrings = {"AQ1": "something"}
@@ -81,7 +168,7 @@ export default class AirCard extends Component {
     // if (currentUser) {
     return (
     <>
-      {this.extractrating(this.props.airRating)}
+      {this.cardResponse(this.props.airRating)}
     </>
 
     );
