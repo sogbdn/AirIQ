f934c3de71304ff03482dbd8b5abc0d6b42126ae
sogbdn <sonia.barretdenazaris@gmail.com>
2019-03-11 23:14:25 +0000

    Modified AirVisual key to be secret and deleted unused function

--- server/sms.js
+++ server/sms.js
@@ -25,7 +25,8 @@ knex.from('users').innerJoin('locations', 'users.location_id', 'locations.id').t
 	users.forEach((user) => {
 		axios
 			.get(
-				`https://api.airvisual.com/v2/city?city=${user.city}&state=${user.state}&country=${user.country}&key=u78aw3FawH3LQxNQx`
+				`https://api.airvisual.com/v2/city?city=${user.city}&state=${user.state}&country=${user.country}&key=${process
+					.env.airVisKey}`
 			)
 			.then((res) => {
 				console.log('aqius from AirVisual', res.data.data.current.pollution.aqius);
@@ -40,7 +41,7 @@ knex.from('users').innerJoin('locations', 'users.location_id', 'locations.id').t
 					newAqius >= 100 &&
 					newAqius < 150 &&
 					user.sms_bad_days === 'true' &&
-					(user.profile_type === 'life_style' || user.profile_type === 'medical')
+					(user.profile === 'life_style' || user.profile === 'medical')
 				) {
 					messagebody = `AIR QUALITY in ${user.city} is ${newAqius}: ALERT (MODERATE RISK). Sensitive groups should avoid prolonged exposure, where possible.`;
 				}
