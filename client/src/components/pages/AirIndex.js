4fb436a0cfb439b83f46b3fbb24f735a4d06b6fc
qwhat <justinmao@live.ca>
2019-03-12 18:56:48 +0000

    fix spelling mistakes and typos

--- client/src/components/pages/AirIndex.js
+++ client/src/components/pages/AirIndex.js
@@ -41,7 +41,7 @@ export default class AirIndex extends Component {
     <Col lg={2} md= {3} sm={4} xs={6}><Card.Img variant="top" src="icon_black.png" className ="avatar" bsPrefix/><Card.Subtitle className="mb-2"><b></b></Card.Subtitle></Col>
     <Col lg={10} md= {9} sm={8} xs={6}>
     <Card.Subtitle className="mb-2">Air Quality Index: 0-50</Card.Subtitle>
-    <div className="Aqi_num"><p className="aqiheadline">Breath deep and travel far and wide.</p></div>
+    <div className="Aqi_num"><p className="aqiheadline">Breathe deep and travel far and wide.</p></div>
     <div align="right">
     <div className="cardicons" align="right"><Image src="forest.svg"/></div>
     </div>
@@ -68,7 +68,7 @@ export default class AirIndex extends Component {
     <Col lg={10} md= {9} sm={8} xs={6}>
     <Card.Subtitle className="mb-2">Air Quality Index: 51-100</Card.Subtitle>
     <div className="Aqi_num">
-    <p className="aqiheadlinedark">An okay day for short excursions, but maybe delay the marathon</p> </div>
+    <p className="aqiheadlinedark">An okay day for short excursions, but maybe delay the marathon.</p> </div>
     <div align="right">
     <div className="cardicons" align="right"><Image src="bench.svg"/></div>
     </div>
@@ -101,7 +101,7 @@ export default class AirIndex extends Component {
     <Col lg={10} md= {9} sm={8} xs={6}>
     <Card.Subtitle className="mb-2">Air Quality Index: 151-200</Card.Subtitle>
     <div className="Aqi_num">
-    <p className="aqiheadlinedark">Take it easy and take shelter. Limit outdoor time or heavy exersions, where possible</p>
+    <p className="aqiheadlinedark">Take it easy and take shelter. Limit outdoor time or heavy exersions where possible.</p>
     </div>
     <div align="right">
     <div className="cardicons" align="right"><Image src="factory4.svg"/></div>
@@ -118,7 +118,7 @@ export default class AirIndex extends Component {
     <Col lg={10} md= {9} sm={8} xs={6}>
     <Card.Subtitle className="mb-2" className="aqiwhitenum">Air Quality Index: 201-300</Card.Subtitle>
     <div className="Aqi_num">
-    <p className="aqiheadline">Hazardous. Serious Serious lung and heart effects present. Take care and stay indoors</p>
+    <p className="aqiheadline">Hazardous. Serious lung and heart effects present. Take care and stay indoors.</p>
     </div>
     <div align="right">
     <div className="cardicons" align="right"><Image src="factory3.svg"/></div>
@@ -136,7 +136,7 @@ export default class AirIndex extends Component {
     <Col lg={10} md= {9} sm={8} xs={6}>
     <Card.Subtitle className="mb-2" className="aqiwhitenum">Air Quality Index: 300+</Card.Subtitle>
     <div className="Aqi_num">
-    <p className="aqiheadline">Lethal Conditions. Serious lung and heart effects that can effect mortality. Everyone should avoid all outdoor exertions.</p>
+    <p className="aqiheadline">Lethal Conditions. Serious lung and heart effects that can affect mortality. Everyone should avoid all outdoor exertion.</p>
     </div>
     <div align="right">
     <div className="cardicons" align="right"><Image src="factory1.svg"/></div>
@@ -154,7 +154,7 @@ export default class AirIndex extends Component {
   <Row>
     <Col>
     <p>The Air Quality Index (AQI) is used for reporting daily air quality. It tells you how clean or polluted your air is, and what associated health effects might be a concern for you. The AQI focuses on health effects you may experience within a few hours or days after breathing polluted air. EPA calculates the AQI for five major air pollutants regulated by the Clean Air Act: ground-level ozone, particle pollution (also known as particulate matter), carbon monoxide, sulfur dioxide, and nitrogen dioxide. For each of these pollutants, EPA has established national air quality standards to protect public health .Ground-level ozone and airborne particles are the two pollutants that pose the greatest threat to human health in this country.</p>
-    
+
     </Col>
     </Row>
   </Card.Body>
